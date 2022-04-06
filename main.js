f15b94127c4a28ad4e1f3833e15ecf903b9fb820
omer <peynirciomerfaruk@gmail.com>
2022-04-06 11:11:38 +0200

    db connection and load  tasks completed

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import axios from "axios";
 
-
-document.querySelector("#app").innerHTML = `
+const appElem=document.querySelector("#app");
+const dbUrl = "http://localhost:3002/todos";
+const response = await axios.get(dbUrl);
+const tasks = response.data;
+console.log(tasks);
+const loadTasks=(tasks)=>{
+appElem.innerHTML = `
 <div class="container-lg pt-4">
 <div class="card mx-auto" style="width: 80%;">
   <div class="card-header">
@@ -14,6 +19,7 @@ document.querySelector("#app").innerHTML = `
     </div>
   </div>
   <ul class="list-group list-group-flush">
+
     <li class="list-group-item">
       <div class="d-flex justify-content-between p-2">
         <div class="form-check">
@@ -27,9 +33,35 @@ document.querySelector("#app").innerHTML = `
           <i class="fa-solid fa-trash text-danger"></i>
         </div>
     </li>
+    ${tasks.map((task) => {
+      return `
+        <li class="list-group-item">
+      <div class="d-flex justify-content-between p-2">
+        <div class="form-check">
+          
+          <input class="form-check-input" type="checkbox" value="" ${
+            task.isCompleted ? "checked" : ""
+          }>
+          <label class="form-check-label" for="flexCheckChecked">
+          ${
+            task.isCompleted ? "<s>" : ""
+          }${task.detail} ${
+            task.isCompleted ? "</s>" : ""
+          }
+          </label>
+
+        </div>
+        <div class="delete-button">
+          <i class="fa-solid fa-trash text-danger"></i>
+        </div>
+    </li>`;
+    })}
   </ul>
 </div>
 
   
   
 `;
+};
+
+loadTasks(tasks);
