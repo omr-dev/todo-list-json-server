797aeaa82afa8be2e1e3c2d80aec0c1bdd85a692
omer <peynirciomerfaruk@gmail.com>
2022-04-06 11:50:55 +0200

    add item created

--- main.js
+++ main.js
@@ -3,38 +3,42 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import axios from "axios";
 
-const appElem=document.querySelector("#app");
+const appElem = document.querySelector("#app");
 const dbUrl = "http://localhost:3002/todos";
-const response = await axios.get(dbUrl);
-const tasks = response.data;
-console.log(tasks);
-const loadTasks=(tasks)=>{
+
 appElem.innerHTML = `
 <div class="container-lg pt-4">
 <div class="card mx-auto" style="width: 80%;">
   <div class="card-header">
     <div class="input-group">
-      <input type="text" class="form-control" placeholder="New item">
+      <input type="text" class="form-control new-task-input" placeholder="New item">
       <button class="btn btn-success" type="button" id="button-add-item">Add Item</button>
     </div>
   </div>
-  <ul class="list-group list-group-flush">
+  <ul class="list-group list-group-flush tasks-list">
+  </ul>
+  </div>`;
 
-    <li class="list-group-item">
-      <div class="d-flex justify-content-between p-2">
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="">
-          <label class="form-check-label" for="flexCheckChecked">
-            test item
-          </label>
+const addTaskElem = document.querySelector("#button-add-item");
+addTaskElem.addEventListener("click", addNewTask);
 
-        </div>
-        <div class="delete-button">
-          <i class="fa-solid fa-trash text-danger"></i>
-        </div>
-    </li>
-    ${tasks.map((task) => {
-      return `
+async function addNewTask() {
+  const newTaskDetails = document.querySelector(".new-task-input").value;
+  await axios.post(dbUrl, {
+    detail: newTaskDetails,
+    isCompleted: false,
+  });
+  document.querySelector(".new-task-input").value = "";
+  loadTasks();
+}
+async function loadTasks() {
+  const response = await axios.get(dbUrl);
+  const tasks = response.data;
+  const tasksListElem = document.querySelector(".tasks-list");
+  tasksListElem.innerHTML = "";
+
+  tasks.map((task) => {
+    tasksListElem.innerHTML += ` 
         <li class="list-group-item">
       <div class="d-flex justify-content-between p-2">
         <div class="form-check">
@@ -43,11 +47,9 @@ appElem.innerHTML = `
             task.isCompleted ? "checked" : ""
           }>
           <label class="form-check-label" for="flexCheckChecked">
-          ${
-            task.isCompleted ? "<s>" : ""
-          }${task.detail} ${
-            task.isCompleted ? "</s>" : ""
-          }
+          ${task.isCompleted ? "<s>" : ""}${task.detail} ${
+      task.isCompleted ? "</s>" : ""
+    }
           </label>
 
         </div>
@@ -55,13 +57,6 @@ appElem.innerHTML = `
           <i class="fa-solid fa-trash text-danger"></i>
         </div>
     </li>`;
-    })}
-  </ul>
-</div>
-
-  
-  
-`;
-};
-
-loadTasks(tasks);
+  });
+}
+loadTasks();
